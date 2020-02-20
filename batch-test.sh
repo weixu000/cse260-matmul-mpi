0122b376f6daa0e268859cf17e75a8f9e2dffec5
Utkarsh Singh <usingh@comet-ln3.sdsc.edu>
2020-02-20 11:54:22 -0800

    starter code

---
+++ batch-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+var=$(sbatch N1_comet-48.slurm | (awk '{print $4}'))
+a=$(date +%s%N)
+echo $var
+state="PENDING"
+while [ "$state" != "RUNNING" ]
+do
+state=$(sacct -j $var | awk 'FNR == 3 {print $6}')
+done
+b=$(date +%s%N)
+diff=$((b-a))
+printf "Queuing Time: %s.%s\n" "${diff:0: -9}" "${diff: -9:3}"
